334106fff342971c55613a47e4fb1a6c0ebedb9e
Leo <leo@bm-sms.com.tw>
2015-11-26 12:03:36 +0800

    add many files

---
+++ lesson4 - Copy (4).php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * PHP basic usage examples.
+ * Lesson 4: Class
+ */
+
+class Foo
+{
+    public $bar;
+    protected $abc = '';        // 給定初始值
+    private $foo;
+    public static $property;    // 靜態成員屬性
+
+    /**
+     * 建購子
+     *
+     * @param $foo
+     */
+    public function __construct($foo)
+    {
+        $this->foo = $foo;
+    }
+
+    public function foo() {
+
+    }
+
+    public static function staticFunction()     // 靜態方法
+    {
+        echo 'this is a static class method.';
+
+        // 可以用 Foo::staticFunction() 的語法做該方法的呼叫
+    }
+}
+
+/**
+ * ExFoo 繼承 Foo
+ *
+ * Class ExFoo
+ */
+class ExFoo extends Foo
+{
+    /**
+     * 初始化母 Class 的建購子
+     *
+     * @param $foo
+     */
+    public function __construct($foo)
+    {
+        parent::__construct($foo);
+    }
+
+    public function test()
+    {
+        //$this->foo;
+    }
+}
+
+/**
+ * Interface Storage
+ */
+interface Storage
+{
+    public function get($key);
+    public function set($key, $value);
+}
+class MyStorage implements Storage
+{
+    public function get($key)
+    {
+        return 'key';
+    }
+
+    public function set($key, $value)
+    {
+        $key = $value;
+    }
+
+    public function myOtherFunction()
+    {
+        echo 'bala bala...';
+    }
+}
+
+// interface 繼承與雙 interface
+interface Student
+{
+    public function study();
+}
+interface Teacher
+{
+    public function teach();
+}
+interface Mix extends Student, Teacher
+{
+
+}
+class Me implements Mix
+{
+    public function study() {
+        echo 'study';
+    }
+
+    public function teach() {
+        echo 'teach';
+    }
+}
+class MyTeacher implements Teacher
+{
+    public function teach() {
+        echo 'teach';
+    }
+}
+$me = new Me();
+$tea = new MyTeacher();
+function test(Student $student, Teacher $teacher) {
+
+}
+test($me, $me);
+// test($tea, $me); // error!
+
+/**
+ * 抽像物件
+ *
+ * 不得直建構抽像物件
+ * 建立一新物件繼承該抽像物件後再建構
+ *
+ * Class AbstractStudent
+ */
+abstract class AbstractStudent
+{
+    protected $name;
+    protected $class;
+
+    public function run()
+    {
+        echo 'run';
+    }
+}
+class EngineerStudent extends AbstractStudent
+{
+    protected $geekLevel;
+}
+class ArtStudent extends AbstractStude
+{
+    protected $fashionLevel;
+}
